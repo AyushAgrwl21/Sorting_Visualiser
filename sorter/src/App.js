86a5010449cb17f8213e0a5c94aa0836f38419c4
Ayush Agarwal <thisismemanayush@gmail.com>
2020-07-30 18:33:19 +0530

    Demo Commit

--- sorter/src/App.js
+++ sorter/src/App.js
@@ -9,7 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          This is a demo commit
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
