a4efff8a777c11bee431308eb9752518b8158818
Ayush Agarwal <thisismemanayush@gmail.com>
2020-08-01 23:28:02 +0530

    Array displayed in browser

--- sorter/src/App.js
+++ sorter/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import SortingVisuals from './SortingVisuals/SortingVisuals';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          This is a demo commit
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SortingVisuals></SortingVisuals>
     </div>
   );
 }
---
+++ sorter/src/SortingVisuals/SortingVisuals.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './SortingVisuals.css';
+
+export default class SortingVisuals extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            array: [],
+        };
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+
+    resetArray() {
+        const array = [];
+        for (let i = 0;i < 100; i++) {
+            array.push(randomIntFromInterval(10,1000));
+        }
+        this.setState({array});
+    }
+
+    render() {
+        const {array} = this.state;
+
+        return (
+            <>
+            {array.map((value, idx) => (
+                <div className="array-bar" key = {idx}>
+                    {value}
+                </div>
+                ))}
+            </>
+        );
+    }
+}
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random() * (max-min+1)+min);
+}
