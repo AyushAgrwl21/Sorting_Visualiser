78b098072a562d7eb454d98e53f2d4dbff272a5b
Ayush Agarwal <thisismemanayush@gmail.com>
2020-08-05 02:47:48 +0530

    Added buttons for different sortings

--- sorter/src/SortingVisuals/SortingVisuals.jsx
+++ sorter/src/SortingVisuals/SortingVisuals.jsx
@@ -22,6 +22,13 @@ export default class SortingVisuals extends React.Component {
         this.setState({array});
     }
 
+    mergeSort() {}
+    quickSort() {}
+    heapSort() {}
+    bubbleSort() {}
+    //insertionSort() {}
+    //selectionSort() {}
+
     render() {
         const {array} = this.state;
 
@@ -35,10 +42,16 @@ export default class SortingVisuals extends React.Component {
                 </div>
                 ))}
                 <button onClick={() => this.resetArray()}>Generate New Array</button>
-            </div>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.quickSort()}>Quick Sort</button>
+                <button onClick={() => this.heapSort()}>Heap Sort</button>
+                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+                </div>
         );
     }
 }
+//<button onClick={() => this.insertionSort()}>Insertion Sort</button>
+//<button onClick={() => this.selectionSort()}>Selection Sort</button>
 
 function randomIntFromInterval(min,max) {
     return Math.floor(Math.random() * (max-min+1)+min);
