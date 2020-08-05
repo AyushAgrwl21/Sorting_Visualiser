583193c6a16bd800e112e02b8ec914ac85bfa03f
Ayush Agarwal <thisismemanayush@gmail.com>
2020-08-06 01:01:52 +0530

    Merge Sort completed

--- sorter/src/SortingVisuals/SortingVisuals.jsx
+++ sorter/src/SortingVisuals/SortingVisuals.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import {getMergeSortAnimations} from '../sortingAlgos/sortingAlgos.js';
 import './SortingVisuals.css';
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 5;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 200;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'purple';
 
 export default class SortingVisuals extends React.Component {
     constructor(props) {
@@ -16,13 +29,35 @@ export default class SortingVisuals extends React.Component {
     // To generate a new array on start and when refreshed
     resetArray() {
         const array = [];
-        for (let i = 0; i <200; i++) {
+        for (let i = 0; i <NUMBER_OF_ARRAY_BARS; i++) {
             array.push(randomIntFromInterval(10,700));
         }
         this.setState({array});
     }
 
-    mergeSort() {}
+    mergeSort() {
+        const animations=getMergeSortAnimations(this.state.array);
+        for (let i=0;i<animations.length;i++) {
+            const arrayBars =document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+                }
+        }
+    }
     quickSort() {}
     heapSort() {}
     bubbleSort() {}
@@ -38,15 +73,18 @@ export default class SortingVisuals extends React.Component {
                 <div 
                     className="array-bar" 
                     key = {idx}
-                    style={{height: `${value}px`}}>
+                    style={{
+                        backgroundColor: PRIMARY_COLOR,
+                        height: `${value}px`,
+                        }}>
                 </div>
                 ))}
-                <button onClick={() => this.resetArray()}>Generate New Array</button>
-                <button onClick={() => this.mergeSort()}>Merge Sort</button>
-                <button onClick={() => this.quickSort()}>Quick Sort</button>
-                <button onClick={() => this.heapSort()}>Heap Sort</button>
-                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
-                </div>
+            <button onClick={() => this.resetArray()}>Generate New Array</button>
+            <button onClick={() => this.mergeSort()}>Merge Sort</button>
+            <button onClick={() => this.quickSort()}>Quick Sort</button>
+            <button onClick={() => this.heapSort()}>Heap Sort</button>
+            <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            </div>
         );
     }
 }
